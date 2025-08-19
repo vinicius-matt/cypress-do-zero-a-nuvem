62f68e3a5c92cbb87a2d9592c64209a5764f7dd2
Matheus V <matheusvini754@gmail.com>
2025-08-19 19:22:12 -0300

    preenchendo formulario

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,8 +1,21 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
 
+  //caso de teste
+  it('verifica o título da aplicação', () => {
     cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+  })
+
+  it.only('Preenche os campos obrigatórios', () =>{
+    cy.get('#firstName').type('Matheus')
+    cy.get('#lastName').type('Miranda')
+    cy.get('#email').type('matheusvini754@gmail.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    //verificações
+    cy.get('.success').should('be.visible')
   })
 })
