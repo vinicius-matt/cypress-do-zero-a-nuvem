e589f87d0f66f4d8afe9ffda2c401f46c650ff42
Matheus V <matheusvini754@gmail.com>
2025-08-19 23:44:50 -0300

    praticando cypress com exercicios

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -8,7 +8,8 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
   })
 
-  it.only('Preenche os campos obrigatórios', () =>{
+  //preenchendo campos
+  it('Preenche os campos obrigatórios', () =>{
     cy.get('#firstName').type('Matheus')
     cy.get('#lastName').type('Miranda')
     cy.get('#email').type('matheusvini754@gmail.com')
@@ -17,5 +18,38 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     //verificações
     cy.get('.success').should('be.visible')
+  })
+
+  it('Testeando Delay',() =>{
+    cy.get('#firstName').type('Matheus',{delay:1000})
+    cy.get('#lastName').type('Miranda')
+    cy.get('#email').type('matheusvini754@gmail.com')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    //verificações
+    cy.get('.success').should('be.visible')
+  })
+
+  //Enviando dados do formulario e validando mensagem de erro
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida',() =>{
+    cy.get('#firstName').type('Matheus')
+    cy.get('#lastName').type('Miranda')
+    cy.get('#email').type('matheusvini754l')
+    cy.get('#open-text-area').type('Teste')
+    cy.get('button[type="submit"]').click()
+
+    //verificações
+    cy.get('.error').should('be.visible')
+    cy.get('.error').should('contain.text','Valide os campos obrigatórios')
+  })
+
+  //Exercicio 02 - Validando dados nos campos
+  it.only('Verificando valor no campo telefone',()=>{
+    cy.get('#phone').as('telefone')
+    cy.get('@telefone').type('47997740921')
+
+    //verficando valor
+    cy.get('@telefone').should('have.value','47997740921')
   })
 })
